3e1aa54910de5d88cd5a89bad92eda2fe91b46d2
Ryan Demboski <ryandemboski7@gmail.com>
2020-02-23 09:45:17 -0700

    Added complete Hwk 5

--- CS-136 (Java)/CS-136 Homework/Project_5/src/package_5/ZellersCongruenceClass.java
+++ CS-136 (Java)/CS-136 Homework/Project_5/src/package_5/ZellersCongruenceClass.java
@@ -6,21 +6,23 @@ public class ZellersCongruenceClass
    {
       private static final int DAYS_IN_WEEK = 7;
       private static final int MONTHS_IN_YEAR = 12;
-      private static final int THIRD_MONTH = 3;
+      //I wasn't sure where to use this constant in my program.
+      //private static final int THIRD_MONTH = 3;
       
       public static void main(java.lang.String[] args)
       {
        String monthStr;  
        String dayStr;
        String yearStr;
-       String day = "lol";
-       String date = "lol";
+       String date;
        
-       int monthInt;
+       int monthInt, January = 1, February = 2;
        int dayInt;
-       int yearInt;
-       int bDayNum = 0;  
-       
+       int yearInt; 
+       int yearPart = 0;
+       int centuryPart = 0;
+       int solve = 0;
+       int userBDay = 0;
        
        monthStr = JOptionPane.showInputDialog("Enter month of birth");
        dayStr = JOptionPane.showInputDialog("Enter day of birth");
@@ -33,71 +35,97 @@ public class ZellersCongruenceClass
        System.out.println("Birthday Program");
        System.out.println("=================\n");
        
-       day = findBirthdayString(bDayNum);
+       yearPart = yearInt % 100;
+       centuryPart = yearInt / 100;
        
-       System.out.println("For the date " + date + ", the person was born"
-             + " on a " + day);
+       if ((monthInt == January) || (monthInt == February))
+          {
+           monthInt = monthInt + MONTHS_IN_YEAR;
+           yearPart -= 1;
+          }
+       
+       
+       solve = (dayInt + findSecondElement(monthInt) + yearPart + 
+             findFourthElement(yearPart) + findFifthElement(centuryPart) + 
+             findSixthElement(centuryPart));
+       
+       userBDay = (solve % DAYS_IN_WEEK);
+       
+       date = findBirthdayString(userBDay);
+       
+       System.out.println("For the date " + monthInt + "/" + dayInt + "/" 
+             + yearInt + ", the person was born" + " on a " + date);
       }
-      
       
       private static int findSecondElement(int month)
       {
-         int January = 1, February = 2;
+         int answer;
          
-         if ((month == January) || (month == February))
-            {
-                 return (month + MONTHS_IN_YEAR);   
-            }
+         answer = (int) (Math.floor((13 * (month + 1) / 5)));
+
+         return answer;
       }
       
       
       private static int findFourthElement(int yearPart)
       {
-         return 0;
+         int answer;
+         
+         answer = (int) (Math.floor((yearPart / 4)));
+         
+         return answer;
       }
       
       
       private static int findFifthElement(int centuryPart)
       {
-         return 0;
+         int answer;
+         
+         answer = (int) (Math.floor((centuryPart / 4)));
+         
+         return answer;
       }
       
       private static int findSixthElement(int centuryPart)
       {
-         return 0;
+         return (5 * centuryPart);
       }
       
       
       private static String findBirthdayString(int birthDayNumber)
       {
+         
+         String dayStr = "";
+
          if (birthDayNumber == 0)
             {
-               return "Saturday";
+               dayStr = "Saturday";
             }
          else if (birthDayNumber == 1)
             {
-               return "Sunday";
+               dayStr = "Sunday";
             }
          else if (birthDayNumber == 2)
             {
-               return "Monday";
+               dayStr = "Monday";
             }
          else if (birthDayNumber == 3)
             {
-               return "Tuesday";
+               dayStr = "Tuesday";
             }
          else if (birthDayNumber == 4)
             {
-               return "Wednesday";
+               dayStr = "Wednesday";
             }
          else if (birthDayNumber == 5)
             {
-               return "Thursday";
+               dayStr = "Thursday";
             }
          else if (birthDayNumber == 6)
             {
-               return "Friday";
+               dayStr = "Friday";
             }
+       return dayStr;
       }
       
       
