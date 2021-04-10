b592d9dc7b8348de44854d75ad2922727f77841c
Ryan Demboski <ryandemboski7@gmail.com>
2021-04-09 23:24:30 -0700

    Updated python

--- Basic Python/CS-126 Homework/Homework 9/hwk9.py
+++ Basic Python/CS-126 Homework/Homework 9/hwk9.py
@@ -5,7 +5,9 @@ csv_year = sys.argv[2]
 with open('summary.txt', 'w') as csv_write, open(sys.argv[1], 'r') as csv_file:
     line_counter = 0
     sorted_lines = []
+
     for line in csv_file.readlines():
+
         if line_counter == 0:
             headers = line.strip().split(',')
             populations = headers.index('Projected Populations')
@@ -13,11 +15,15 @@ with open('summary.txt', 'w') as csv_write, open(sys.argv[1], 'r') as csv_file:
             states = headers.index("State")
             line_counter += 1
             continue
+
         else:
             line = line.split(',')
             line = [col.strip() for col in line]
+
             if str(line[years]).strip() == csv_year:
                 sorted_lines.append(line)
+
     sorted_lines.sort(key = lambda x: x[states].lower())
+    
     for line in sorted_lines:
         csv_write.write(line[states].title().replace('Of ', 'of ') + ': ' + line[populations] + '\n')
